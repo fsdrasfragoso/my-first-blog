47cf55a130c7e6ae770f8cc7342a1bf8f4dd4167
Esdras Fragoso <esdrasfragoso@gmail.com>
2017-05-19 21:57:34 -0300

    banco

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'tianalva',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
