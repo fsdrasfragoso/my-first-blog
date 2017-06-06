1d6a7fe0fcc29a71e4116be221800725a088ec85
Esdras Fragoso <esdrasfragoso@gmail.com>
2017-06-06 13:00:13 -0300

    formulario

--- mysite/TiaNalva/views.py
+++ mysite/TiaNalva/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def index(request):
-	return render(request, 'index.html')
+from .forms import MeuForm
+
+
+def home(request):
+ 
+    # Cria form
+    form = MeuForm(request.POST or None)
+ 
+    # Valida e salva
+    if form.is_valid():
+        inep = form.field(commit='INEP')
+        nome = form.field(commit='Nome da Escola')
+        salvar = form.save(commit=False)
+        salvar.save()
+        return HttpResponse("Dados inseridos com sucesso!")        
+ 
+    # Chama Template
+    return render_to_response("index.html",
+                              locals(),
+                              context_instance = RequestContext(request))
+
+
+
+
+
 
 # Create your views here.
