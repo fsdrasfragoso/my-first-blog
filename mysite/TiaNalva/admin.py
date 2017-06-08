41bef5e8ce768a1204988deae776b1d99777d652
Esdras Fragoso <esdrasfragoso@gmail.com>
2017-06-08 19:05:57 -0300

    admin

--- mysite/TiaNalva/admin.py
+++ mysite/TiaNalva/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Escola
+
+class EscolaAdmin(admin.ModelAdmin):
+	list_display = ['inep', 'nome']
+	search_fields = ['inep', 'nome']
+
+admin.site.register(Escola, EscolaAdmin)
+
 # Register your models here.
--- mysite/TiaNalva/models.py
+++ mysite/TiaNalva/models.py
@@ -1,15 +1,55 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
-#
-# Also note: You'll have to insert the output of 'django-admin sqlcustom [app_label]'
-# into your database.
+
 from __future__ import unicode_literals
 
 from django.db import models
+
+class EscolaManager(models.Manager):
+	"""docsCouseEscola models.Managerame"""
+	def search(self, query):
+		return self.get_queryset().filter(
+			models.Q(inep_icontains=query) | \
+			models.Q(nome_icontains=query)
+		)
+		
+		
+
+class Aluno(models.Model):
+    matricula = models.CharField(max_length=45)
+    nome = models.CharField(db_column='Nome', max_length=45)  # Field name made lowercase.
+    chamada = models.IntegerField()
+    #turma_idturma = models.ForeignKey('Turma', db_column='Turma_idTurma')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'aluno'
+       # unique_together = (('matricula', 'Turma_idTurma'),)
+
+
+
+
+class Assunto(models.Model):
+    
+    assunto = models.CharField(db_column='Assunto', max_length=45)  # Field name made lowercase.
+    descricao = models.CharField(db_column='Descricao', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    #disciplina_iddisciplina = models.ForeignKey('Disciplina', db_column='Disciplina_idDisciplina')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'assunto'
+       # unique_together = (('idAssunto', 'Disciplina_idDisciplina'),)
+
+
+
+
+class Disciplina(models.Model):
+   # iddisciplina = models.AutoField(db_column='idDisciplina')  # Field name made lowercase.
+    nome = models.CharField(db_column='Nome', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    #turma_idturma = models.ForeignKey('Turma', db_column='Turma_idTurma')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'disciplina'
+       # unique_together = (('idDisciplina', 'Turma_idTurma'),)
 
 
 
@@ -23,4 +63,159 @@ class Escola(models.Model):
         db_table = 'escola'
 
 
+class Medias(models.Model):
+  #  id = models.AutoField()
+    primeiro_bimestre = models.FloatField()
+    segundo_bimestre = models.FloatField()
+    terceiro_bimestre = models.FloatField()
+    quarto_bimestre = models.FloatField()
+    resultado = models.FloatField(blank=True, null=True)
+   # aluno_has_disciplina = models.ForeignKey(AlunoHasDisciplina, db_column='Aluno_has_Disciplina_id')  # Field name made lowercase.
 
+    class Meta:
+        managed = False
+        db_table = 'medias'
+    #    unique_together = (('id', 'Aluno_has_Disciplina_id'),)
+
+
+class Nota01(models.Model):
+   # idnota01 = models.AutoField(db_column='idNota01')  # Field name made lowercase.
+    nota = models.FloatField()
+    #aluno_has_disciplina = models.ForeignKey(AlunoHasDisciplina, db_column='Aluno_has_Disciplina_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'nota01'
+     #   unique_together = (('idNota01', 'Aluno_has_Disciplina_id'),)
+
+
+class Nota02(models.Model):
+ #   id = models.AutoField()
+    nota = models.FloatField(db_column='Nota')  # Field name made lowercase.
+ #   aluno_has_disciplina = models.ForeignKey(AlunoHasDisciplina, db_column='Aluno_has_Disciplina_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'nota02'
+  #      unique_together = (('id', 'Aluno_has_Disciplina_id'),)
+
+
+class Nota03(models.Model):
+  #  id = models.AutoField()
+    nota = models.FloatField()
+   # aluno_has_disciplina = models.ForeignKey(AlunoHasDisciplina, db_column='Aluno_has_Disciplina_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'nota03'
+    #    unique_together = (('id', 'Aluno_has_Disciplina_id'),)
+
+
+class Nota04(models.Model):
+   # id = models.AutoField()
+    nota = models.FloatField()
+   # aluno_has_disciplina = models.ForeignKey(AlunoHasDisciplina, db_column='Aluno_has_Disciplina_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'nota04'
+    #    unique_together = (('id', 'Aluno_has_Disciplina_id'),)
+
+
+class Nota05(models.Model):
+  #  id = models.AutoField()
+    nota = models.FloatField()
+   # aluno_has_disciplina = models.ForeignKey(AlunoHasDisciplina, db_column='Aluno_has_Disciplina_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'nota05'
+    #    unique_together = (('id', 'Aluno_has_Disciplina_id'),)
+
+
+class Nota06(models.Model):
+  #  id = models.AutoField()
+    nota = models.FloatField()
+   # aluno_has_disciplina = models.ForeignKey(AlunoHasDisciplina, db_column='Aluno_has_Disciplina_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'nota06'
+    #    unique_together = (('id', 'Aluno_has_Disciplina_id'),)
+
+
+class Nota07(models.Model):
+   # id = models.AutoField()
+    nota = models.FloatField(db_column='Nota')  # Field name made lowercase.
+    #aluno_has_disciplina = models.ForeignKey(AlunoHasDisciplina, db_column='Aluno_has_Disciplina_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'nota07'
+     #   unique_together = (('id', 'Aluno_has_Disciplina_id'),)
+
+
+class Nota08(models.Model):
+   # id = models.AutoField()
+    nota = models.CharField(db_column='Nota', max_length=45)  # Field name made lowercase.
+   # aluno_has_disciplina = models.ForeignKey(AlunoHasDisciplina, db_column='Aluno_has_Disciplina_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'nota08'
+    #    unique_together = (('id', 'Aluno_has_Disciplina_id'),)
+
+
+class Questao(models.Model):
+   # idquestao = models.AutoField(db_column='idQuestao')  # Field name made lowercase.
+    pergunta = models.CharField(max_length=5000)
+    resposta = models.CharField(db_column='Resposta', max_length=5000)  # Field name made lowercase.
+    score = models.FloatField(db_column='Score')  # Field name made lowercase.
+    nivel = models.IntegerField(db_column='Nivel')  # Field name made lowercase.
+    #assunto_idassunto = models.ForeignKey(Assunto, db_column='Assunto_idAssunto')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'questao'
+     #   unique_together = (('idQuestao', 'Assunto_idAssunto'),)
+
+
+class Ranking(models.Model):
+  #  idranking = models.AutoField(db_column='idRanking')  # Field name made lowercase.
+    pontuacao = models.FloatField()
+  #  aluno_matricula = models.ForeignKey(Aluno, db_column='ALUNO_matricula')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'ranking'
+   #     unique_together = (('idRanking', 'ALUNO_matricula'),)
+
+
+class Turma(models.Model):
+   # idturma = models.AutoField(db_column='idTurma')  # Field name made lowercase.
+    nome = models.CharField(db_column='Nome', max_length=45, blank=True, null=True)  # Field name made lowercase.
+  #  escola_inep = models.ForeignKey(Escola, db_column='ESCOLA_inep')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'turma'
+   #     unique_together = (('idTurma', 'ESCOLA_inep'),)
+
+
+class Usuario(models.Model):
+    #idusuario = models.AutoField(db_column='idUsuario')  # Field name made lowercase.
+    login = models.CharField(max_length=45)
+    senha = models.CharField(max_length=45)
+    nivel = models.IntegerField()
+    cadastro = models.DateField(blank=True, null=True)
+    #escola_inep = models.ForeignKey(Escola, db_column='ESCOLA_inep')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'usuario'
+     #   unique_together = (('idUsuario', 'ESCOLA_inep'),)
+
+objects = EscolaManager()
+
+def _str_(self):
+	return self.inep
